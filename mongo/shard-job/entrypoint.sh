405600f8abe3623ed585d2f28d3098763d3ee70b
Даниил Жиров <daniilzhyrov@Daniils-MacBook-Pro.local>
2021-05-23 21:34:21 +0300

    8

--- mongo/shard-job/entrypoint.sh
+++ mongo/shard-job/entrypoint.sh
@@ -1,5 +1,11 @@
 kubectl wait --for=condition=ready pod -n $MONGOS_NAMESPACE -l role=mongos
 
-kubectl exec -n $MONGOS_NAMESPACE -l role=mongos -it -- bash
+mongo --host $MONGOS_SVC_NAME.$MONGOS_NAMESPACE.svc.cluster.local
 
-mongo --port 43000 --bind_ip localhost
+sh.addShard("rs0/rs0-0.rs0.$RS_NAMESPACE.svc.cluster.local:27017, rs0-1.rs0.$RS_NAMESPACE.svc.cluster.local:27017, rs0-2.rs0.$RS_NAMESPACE.svc.cluster.local:27017")
+sh.addShard("rs1/rs1-0.rs1.$RS_NAMESPACE.svc.cluster.local:27017, rs1-1.rs1.$RS_NAMESPACE.svc.cluster.local:27017, rs1-2.rs1.$RS_NAMESPACE.svc.cluster.local:27017")
+
+sh.enableSharding("$SHARD_DATABASE_NAME")
+use $SHARD_DATABASE_NAME
+
+db.$SHARD_DATABASE_NAME.createIndex( { number : 1 } )
