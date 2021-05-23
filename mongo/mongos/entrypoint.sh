af62b43404a5b0b42f126100fea7bab395dcfabb
Даниил Жиров <daniilzhyrov@Daniils-MacBook-Pro.local>
2021-05-24 00:45:28 +0300

    8

--- mongo/mongos/entrypoint.sh
+++ mongo/mongos/entrypoint.sh
@@ -19,15 +19,15 @@ fi
 kubectl rollout status --watch --timeout=600s statefulset/cfg-svr -n kursach
 
 printf "rs.initiate( { _id: "configReplSet", configsvr: true, members: [
-    { _id: 0, host: "$CFG_SVR_NAME-0.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local:27017" },
-    { _id: 1, host: "$CFG_SVR_NAME-1.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local:27017" },
-    { _id: 2, host: "$CFG_SVR_NAME-2.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local:27017" },
+    { _id: \"0\", host: \"$CFG_SVR_NAME-0.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local:27017\" },
+    { _id: \"1\", host: \"$CFG_SVR_NAME-1.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local:27017\" },
+    { _id: \"2\", host: \"$CFG_SVR_NAME-2.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local:27017\" },
 ]})\nexit"
 
 printf "rs.initiate( { _id: "configReplSet", configsvr: true, members: [
-    { _id: 0, host: "$CFG_SVR_NAME-0.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local:27017" },
-    { _id: 1, host: "$CFG_SVR_NAME-1.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local:27017" },
-    { _id: 2, host: "$CFG_SVR_NAME-2.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local:27017" },
+    { _id: \"0\", host: \"$CFG_SVR_NAME-0.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local:27017\" },
+    { _id: \"1\", host: \"$CFG_SVR_NAME-1.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local:27017\" },
+    { _id: \"2\", host: \"$CFG_SVR_NAME-2.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local:27017\" },
 ]})\nexit" | mongo --host $CFG_SVR_NAME-0.$CFG_SVR_SVC.$CFG_SVR_NAMESPACE.svc.cluster.local
 
 
