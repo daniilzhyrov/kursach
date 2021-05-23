ff93c597d3c71a4a37d80d6a078a9bc59258443f
Даниил Жиров <daniilzhyrov@Daniils-MacBook-Pro.local>
2021-05-24 01:27:56 +0300

    8

--- mongo/shard-job/entrypoint.sh
+++ mongo/shard-job/entrypoint.sh
@@ -2,8 +2,8 @@ kubectl rollout status --watch --timeout=600s statefulset/cfg-svr -n kursach
 
 sleep 12
 
-printf "sh.addShard(\"rs0/rs0-0.rs0.$RS_NAMESPACE.svc.cluster.local:27017, rs0-1.rs0.$RS_NAMESPACE.svc.cluster.local:27017, rs0-2.rs0.$RS_NAMESPACE.svc.cluster.local:27017\")
-sh.addShard(\"rs1/rs1-0.rs1.$RS_NAMESPACE.svc.cluster.local:27017, rs1-1.rs1.$RS_NAMESPACE.svc.cluster.local:27017, rs1-2.rs1.$RS_NAMESPACE.svc.cluster.local:27017\")
+printf "sh.addShard(\"rs0/rs0-0.rs0.$RS_NAMESPACE.svc.cluster.local:27017\")
+sh.addShard(\"rs1/rs1-0.rs1.$RS_NAMESPACE.svc.cluster.local:27017\")
 sh.enableSharding(\"$SHARD_DATABASE_NAME\")
 use $SHARD_DATABASE_NAME
 db.$SHARD_DATABASE_NAME.createIndex( { number : 1 } )
